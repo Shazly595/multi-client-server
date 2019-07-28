e0f056b26fcad9a30e7adba99d32b9ebb4f51a6f
Shazly595 <Shazly_Prog@hotmail.com>
2019-07-28 03:17:59 +0200

    Update server.c

--- server.c
+++ server.c
@@ -6,58 +6,45 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-
 #define PORT 4444
 
 int main(){
-
 	int sockfd, ret;
-	 struct sockaddr_in serverAddr;
-
+	struct sockaddr_in serverAddr;
 	int newSocket;
 	struct sockaddr_in newAddr;
-
 	socklen_t addr_size;
-
 	char buffer[1024];
 	pid_t childpid;
-
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if(sockfd < 0){
 		printf("[-]Error in connection.\n");
 		exit(1);
 	}
 	printf("[+]Server Socket is created.\n");
-
 	memset(&serverAddr, '\0', sizeof(serverAddr));
 	serverAddr.sin_family = AF_INET;
 	serverAddr.sin_port = htons(PORT);
 	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-
 	ret = bind(sockfd, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
 	if(ret < 0){
 		printf("[-]Error in binding.\n");
 		exit(1);
 	}
 	printf("[+]Bind to port %d\n", 4444);
-
 	if(listen(sockfd, 10) == 0){
 		printf("[+]Listening....\n");
 	}else{
 		printf("[-]Error in binding.\n");
 	}
-
-
 	while(1){
 		newSocket = accept(sockfd, (struct sockaddr*)&newAddr, &addr_size);
 		if(newSocket < 0){
 			exit(1);
 		}
 		printf("Connection accepted from %s:%d\n", inet_ntoa(newAddr.sin_addr), ntohs(newAddr.sin_port));
-
 		if((childpid = fork()) == 0){
 			close(sockfd);
-
 			while(1){
 				recv(newSocket, buffer, 1024, 0);
 				if(strcmp(buffer, ":exit") == 0){
@@ -70,11 +57,7 @@ int main(){
 				}
 			}
 		}
-
 	}
-
 	close(newSocket);
-
-
 	return 0;
 }
